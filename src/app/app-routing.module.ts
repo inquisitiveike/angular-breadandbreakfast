6b1c20e95ba5b804f7cf5b0a91801348252ffe66
inquisitiveike <mnfleming7@gmail.com>
2020-03-27 09:15:15 -0600

    4 files modified, 4 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
